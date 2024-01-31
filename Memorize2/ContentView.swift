e33c0d215315b106a61248bfc1537904b0329c92
Lucas <lucasofjus@gmail.com>
2024-01-31 18:30:10 -0300

    Init Commit

--- Memorize2/ContentView.swift
+++ Memorize2/ContentView.swift
@@ -9,16 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView()
+            CardView(isFaceUp: true )
+            CardView()
+            CardView()
+            
+            
         }
+        .foregroundColor(.orange)
         .padding()
+    
+        
     }
 }
 
-#Preview {
-    ContentView()
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(Color.orange, lineWidth: 2)
+                    .background(Color.white)
+                    .frame(width: (80), height: 120)
+                
+                
+                Text(  "🐸" )
+                    .font(.largeTitle)
+                    .foregroundColor(.orange)
+                    .imageScale(.small)
+                
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.red)
+            }
+        }
+    }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
