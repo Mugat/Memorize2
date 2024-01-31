2d2e78dda1c97ed34aefcab7490b02639759cad1
Lucas <lucasofjus@gmail.com>
2024-01-31 18:28:32 -0300

    Initial Commit

---
+++ Memorize2/Memorize2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2App.swift
+//  Memorize2
+//
+//  Created by Lucas Oliveira Ferreira on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
